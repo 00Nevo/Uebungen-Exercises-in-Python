b6a75f8779717b7aed7b47fdf679814c2f0fcdfa
Gravity Online <albertoborgato9@gmail.com>
2025-03-19 23:20:52 +0100

    Create Ch3.py

---
+++ Ch3.py
@@ -0,0 +1,65 @@
+##
+
+CHAPTER 3
+How You Run Programs
+The Interactive Prompt
+
+Running Code Interactively
+
+Why the Interactive Prompt?
+
+Experimenting
+
+Testing
+
+Using the Interactive Prompt
+
+Entering multiline statements
+
+System Command Lines and Files
+
+A First Script
+
+Running Files with Command Lines
+
+Using Command Lines and Files
+
+Unix Executable Scripts (#!)
+
+Clicking File Icons
+
+Clicking Icons on Windows
+
+The input Trick
+
+Other Icon-Click Limitations
+
+Module Imports and Reloads
+
+The Grander Module Story: Attributes
+
+Modules and namespaces
+
+import and reload Usage Notes
+
+Using exec to Run Module Files
+
+The IDLE User Interface
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
