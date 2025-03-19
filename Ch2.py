594181c0e115023a463dd80c645d5fb8c7634f5c
Gravity Online <albertoborgato9@gmail.com>
2025-03-19 22:03:31 +0100

    Create Ch2.py

---
+++ Ch2.py
@@ -0,0 +1,24 @@
+##I read and worked on following parts of the book.
+
+CHAPTER 2
+
+How Python Runs Programs
+Introducing the Python Interpreter
+Program Execution
+The Programmer’s View
+Python’s View
+Byte code compilation
+The Python Virtual Machine (PVM)
+Performance implications
+Development implications
+Execution Model Variations
+Python Implementation Alternatives
+CPython
+Jython
+Execution Optimization Tools
+The Psyco just-in-time compiler
+The Shedskin C++ translator
+Frozen Binaries
+Future Possibilities?
+Chapter Summary
+Test Your Knowledge: Quiz
