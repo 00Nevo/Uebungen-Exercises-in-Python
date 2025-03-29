3ed306318238adbe3a80b18cd97789ce5e3242de
NEVO <albertoborgato9@gmail.com>
2025-03-29 20:31:52 +0100

    Create Book1-Ch2.py

---
+++ Game-Development-With-Python/Book1-Ch2.py
@@ -0,0 +1,21 @@
+2
+Writing Programs
+
+String Values
+String Concatenation
+Writing Programs in IDLE’s File Editor
+Creating the Hello World Program
+Saving Your Program
+Running Your Program
+How the Hello World Program Works
+Comments for the Programmer
+Functions: Mini-Programs Inside Programs
+The print() Function
+The input() Function
+Expressions in Function Calls
+The End of the Program
+Naming Variables
+Summary
+
+3
+Guess the Number
