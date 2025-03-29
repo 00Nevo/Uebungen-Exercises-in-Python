0bc6107cbe9227d4affc85eedaf84d70f4845f65
NEVO <albertoborgato9@gmail.com>
2025-03-29 20:08:15 +0100

    Create Book1-Ch1.py

---
+++ Game-Development-With-Python/Book1-Ch1.py
@@ -0,0 +1,23 @@
+Invent your own games with python.
+
+Introduction
+Who Is This Book For?
+About This Book
+How to Use This Book
+Line Numbers and Indentation
+Long Code Lines
+Downloading and Installing Python
+Starting IDLE
+Finding Help Online
+1
+The Inter active Shell
+Some Simple Math
+Integers and Floating-Point Numbers
+Expressions
+Evaluating Expressions
+Syntax Errors
+Storing Values in Variables
+Summary
+
+2
+Writing Programs
