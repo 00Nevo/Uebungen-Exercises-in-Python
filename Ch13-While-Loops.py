a8975375dec2cdea771c414a506492ac68f67d1c
NEVO <albertoborgato9@gmail.com>
2025-04-07 18:38:10 +0200

    Create Ch13-While-Loops.py

---
+++ Ch13-While-Loops.py
@@ -0,0 +1,32 @@
+CHAPTER 13
+while and for Loops
+
+while Loops
+General Format
+Examples
+break, continue, pass, and the Loop else
+General Loop Format
+continue
+break
+Loop else
+for Loops
+General Format
+Examples
+  Basic usage
+  Other data types
+  Tuple assignment in for loops
+  Python 3.0 extended sequence assignment in for loops
+  Nested for loops
+Loop Coding Techniques
+Counter Loops: while and range
+Nonexhaustive Traversals: range and Slices
+Changing Lists: range
+Parallel Traversals: zip and map
+   map equivalence in Python 2.6
+   Dictionary construction with zip
+Generating Both Offsets and Items: enumerate
+Chapter Summary
+Test Your Knowledge: Quiz
+
+CHAPTER 14
+Iterations and Comprehensions, Part 1
