afbefc750d731f47a4ee00fb318ffef053d8cd90
NEVO <albertoborgato9@gmail.com>
2025-04-16 10:29:59 +0200

    Create Software Testing Ch1.py

---
+++ Software Testing Ch1.py
@@ -0,0 +1,15 @@
+Chapter 1
+A Perspective on Testing
+
+1.1 Basic Definitions
+1.2 Test Cases
+1.3 Insights from a Venn Diagram
+1.4 Identifying Test Cases
+    1.4.1 Specification-Based Testing
+    1.4.2 Code-Based Testing
+    1.4.3 Specification-Based versus Code-Based Debate
+1.5 Fault Taxonomies
+1.6 Levels of Testing
+
+Chapter 2 
+Examples
