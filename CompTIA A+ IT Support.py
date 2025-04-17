29d5cd9ceeee17975a956849e0ec914accc4419a
NEVO <albertoborgato9@gmail.com>
2025-04-17 18:51:26 +0200

    Create CompTIA A+ IT Support.py

---
+++ CompTIA A+ IT Support.py
@@ -0,0 +1,41 @@
+CompTIA A+ 
+Guide to IT Technical Support
+
+Chapter 1
+Taking a Computer Apart
+and Putting It Back Together
+
+Exploring a Desktop Computer
+
+Step 1: Planning and Organizing Your Work and Gathering Your Tools
+Tools Used by a Computer Hardware Technician
+
+Step 2: Opening the Case
+What’s on the Outside of a Desktop Case
+Loopback Plugs
+What’s Inside a Desktop Case
+Form Factors Used By Desktop Cases, Pow er Supplies,
+and Motherboards
+
+Step 3: Removing Expansion Cards
+
+Step 4: Removing the Motherboard
+
+POST Diagnostic Cards
+
+Step 5: Removing the Pow er Supply
+Power Supply Tester
+Steps to Put a Computer Back To gether
+First Look at Laptop Components
+Docking Stations and Port Replicators
+Special Considerations When Supporting Laptops
+Warranty Concerns
+Service Manuals and Other Sources of Information
+Diagnostic Tools Provided by Manufacturers
+Working Inside a Laptop Computer
+Exploring Laptop Internal Components
+Exploring Inside an All-in-One Computer
+Exploring a Desktop Computer
+
+Chapter 2
+All About Motherboards
