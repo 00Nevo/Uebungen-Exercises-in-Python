5114c78f943d1856569dfdf6b698869d1059a1ed
NEVO <albertoborgato9@gmail.com>
2025-04-19 19:47:34 +0200

    Create Ch2.py

---
+++ Ch2.py
@@ -0,0 +1,25 @@
+Chapter 2 Examples
+
+2.1 Generalized Pseudocode
+2.2 The Triangle Problem
+2.2.1 Problem Statement
+2.2.2 Discussion
+2.2.3 Traditional Implementation
+2.2.4 Structured Implementations
+2.3 The NextDate Function
+2.3.1 Problem Statement
+2.3.2 Discussion
+2.3.3 Implementations
+2.4 The Commission Problem
+2.4.1 Problem Statement
+2.4.2 Discussion
+2.4.3 Implementation
+2.5 The SATM System
+2.5.1 Problem Statement
+2.5.2 Discussion
+2.6 The Currency Converter
+2.7 Saturn Windshield Wiper Controller
+2.8 Garage Door Opener
+
+Chapter 3
+Discrete Math for Testers
