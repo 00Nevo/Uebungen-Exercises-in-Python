a3a47071675b08d922c331d85d71e83703c80ef0
NEVO <albertoborgato9@gmail.com>
2025-05-06 11:14:29 +0200

    Create Ch1.py

---
+++ Ch1.py
@@ -0,0 +1,20 @@
+Introduction to Digital Music with Python Programming
+by Michael S. Horn,Melanie West and Cameron Roberts.
+
+1 Why music and coding?
+1.1 What is Python?
+1.2 What this book is not
+1.3 What this book is
+1.4 TunePad and EarSketch
+1.5 A quick example
+1.6 Five reasons to learn code
+1.6.1 REASON 1: Like it or note, music is already defned by code
+1.6.2 REASON 2: Code is a powerful way to make music
+1.6.3 REASON 3: Code lets you build your own musical toolkit
+1.6.4 REASON 4: Code is useful for a thousand and one other things
+1.6.5 REASON 5: Coding makes us more human
+1.7 The future of music and code
+1.8 Book overview
+
+Interlude 1
+BASIC POP BEAT
